47a58df1a9a3b4dcbf5858fa1c00242a73c208b4
jannunzi <jannunzi@gmail.com>
2019-07-16 22:24:41 -0400

    start code for assignment

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ var app = express()
 
 var helloController = require('./controllers/HelloController.js')(app)
 
+var universityService = require(
+	'./services/university.service.server.js')
+universityService(app)
+
 app.listen(3000)
