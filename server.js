9eeec5e042ccfc676ac08efe3d8583b505fc2b65
jannunzi <jannunzi@gmail.com>
2019-07-18 22:26:21 -0400

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var express = require('express')
 var app = express()
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/wbdv-su19', {useNewUrlParser: true});
+
+var pageSchema = mongoose.Schema({
+	title: String
+}, {collection: 'pages'});
+
+var pageModel = mongoose.model('PageModel', pageSchema)
+
+pageModel.find()
+	.then(pages => console.log(pages))
+
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 var helloController = require('./controllers/HelloController.js')(app)
 
--- services/university.service.server.js
+++ services/university.service.server.js
@@ -1,23 +1,32 @@
 module.exports = function(app) {
-	var students = require('../data/students.json');
-	var questions = require('../data/questions.json');
-	var answers = require('../data/answers.json');
+	var universityDao = require('../daos/university.dao.server.js')
+
+	function createStudent(req, res) {
+		var student = req.body;
+		res.json(universityDao.createStudent(student))
+	}
+	function deleteStudent(req, res) {
+		var sid = req.params['sid'];
+		res.json(universityDao.deleteStudent('sid'))
+	}
 	function findAllStudents(req, res) {
-		res.json(students);
+		res.json(universityDao.findAllStudents());
 	}
 	function findStudentById(req, res) {
 		res.json(
-			students.find(student => student._id == req.params['sid'])
-			)
+			universityDao.findStudentById(req.params['sid'])
+		)
 	}
 	function findAllQuestions(req, res) {
-		res.json(questions);
+		res.json(universityDao.findAllQuestions());
 	}
 	function findQuestionById(req, res) {
 		res.json(
-			questions.find(question => question._id == req.params['qid'])
-			)
+			universityDao.findQuestionById(req.params['qid'])
+		)	
 	}
+	app.post("/api/students", createStudent);
+	app.delete("/api/students/:sid", deleteStudent);
 	app.get("/api/students", findAllStudents);
 	app.get("/api/students/:sid", findStudentById);
 	app.get("/api/questions", findAllQuestions);
